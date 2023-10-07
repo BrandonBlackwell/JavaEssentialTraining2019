8b6f1b0379f323fda66f72539947521228aff287
Brandon Blackwell <brandonb13195@gmail.com>
2023-10-06 23:34:29 -0700

    Getting input from user challenge.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) 
+    {
+        // write your code here
+        // d1
+        System.out.print("Enter a numeric value: ");
+        try (Scanner scanner = new Scanner(System.in)) 
+        {
+            double d1 = scanner.nextDouble();
+
+            //d2
+            System.out.print("Enter a numeric value: ");
+            try (Scanner scannerTwo = new Scanner(System.in)) 
+            {
+                double d2 = scannerTwo.nextDouble();
+
+                // Divide and print result
+                System.out.println("The answer is: " + d1/d2);
+            }
+        }
     }
 }
