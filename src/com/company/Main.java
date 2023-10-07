fd3d58af870767689e9e39d9d5bcb28db720922b
Brandon Blackwell <brandonb13195@gmail.com>
2023-10-06 21:53:09 -0700

    Number formatters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        // Number formatters
+        // Create an instance of the formatter then use it on variable.
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number: " + numF.format(doubleValue));
+
+        var intF = NumberFormat.getInstance();
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number: " + intF.format(doubleValue));
+
+        var locale = Locale.getDefault();
+        var localeFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number: " + localeFormatter.format(doubleValue));
     }
 }
